d0cedc4ff46315cf9539b4cf852a89065d5eb0c1
JuanMiranda1998 <mirandajuan1998@outlook.com>
2024-05-25 23:57:52 -0300

    removed components and added functions and fonts

--- src/components/ConfirmationMessage.jsx
+++ src/components/ConfirmationMessage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ConfirmationMessage = () => {
   return (
-    <div className='hidden'>
+    <div className='absolute top-0'>
         <h1>Message Sent!</h1>
         <p>Thanks for completing the form. We'll be in touch soon!</p>
     </div>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,20 +1,89 @@
-import FormCheckField from "./FormCheckField"
-import FormEmailField from "./FormEmailField"
-import FormMessageField from "./FormMessageField"
-import FormNameField from "./FormNameField"
-import FormOptionField from "./FormOptionField"
+import { useState } from "react"
+import ConfirmationMessage from "./ConfirmationMessage"
 
 
 const Form = () => {
+    const [emailSubmitted, setEmailSubmitted] = useState(false)
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        message: ''
+    })
+    const [errors, setErrors] = useState({
+        errorFirstName: null,
+        errorLastName: null,
+        errorEmail: null,
+        errorMessage: null,
+    })
+
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const verifiedFields = verifyData(formData);
+        if (verifiedFields){
+            setEmailSubmitted(true);
+            setFormData({
+                firstName: '',
+                lastName: '',
+                email: '',
+                message: ''
+            });
+        }
+    }
+
+
+    const verifyData = (data) => {
+        return true; 
+    }
+
+
     return (
-        <form>
+        <form className="relative">
             <h1 className='text-2xl mb-4'>Contact Us</h1>
-            <FormNameField />
-            <FormEmailField />
-            <FormOptionField />
-            <FormMessageField />
-            <FormCheckField />
-            <button>Submit</button>
+            <div className='w-full flex flex-col lg:flex-row justify-center items-center'>
+                <div className="w-full flex flex-col justify-center items-start">
+                    <label htmlFor="firstName">First Name</label>
+                    <input className='w-full py-2 pl-4' type="text" id="firstName" name="firstName" />
+                    <p className='hidden'>This field is required</p>
+                </div>
+                <div className="w-full flex flex-col justify-center items-start">
+                    <label htmlFor="lastName">Last Name</label>
+                    <input className='w-full py-2 pl-4' type="text" id="lastName" name="lastName" />
+                    <p className='hidden'>This field is required</p>
+                </div>
+            </div>
+            <div>
+                <label htmlFor="email">Email Address</label>
+                <input name="email" type="text" id="email" />
+                <p className='hidden'>Please enter a valid email address</p>
+                <p className='hidden'>This field is required</p>
+            </div>
+            <div>
+                <label htmlFor="">Query Type</label>
+                <div className='flex flex-col lg:flex-row gap-4 my-4'>
+                    <div className='flex flex-row'><input type="radio" name="queryType" id="" /><p className='ml-2'>General Enquiry</p></div>
+                    <div className='flex flex-row'><input type="radio" name="queryType" id="" /><p className='ml-2'>Support Request</p></div>
+                </div>
+                <p className='hidden'>Please select a query type</p>
+            </div>                              
+            <div>
+                <label htmlFor="message">Message</label>
+                <textarea className='w-full' id="message" rows="5" name="message"></textarea>
+                <p className='hidden'>This field is required</p>
+            </div>
+            <div>
+                <div className='flex flex-row'>
+                    <input type="checkbox" name="consent" id="consent" />
+                    <p className='ml-4'>I consent to being contacted by the team</p>
+                </div>
+                <p className='hidden'>To submit this form, please consent to being contacted</p>
+            </div>
+            <button onClick={handleSubmit}>Submit</button>
+            {emailSubmitted&&<ConfirmationMessage />}
         </form>
     )
 }
