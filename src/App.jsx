b2058c94c2db314f704a79206078f2e1419b2095
JuanMiranda1998 <mirandajuan1998@outlook.com>
2024-05-26 00:48:22 -0300

    added styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Form from './components/Form'
 function App() {
 
   return (
-    <div className='w-[100vw] min-h-[100vh] bg-[#e0f1e7] flex flex-col justify-center items-center font-body'>
-      <div className='w-[95%] max-w-[600px] my-8 px-8 py-10 bg-white flex flex-col justify-center items-start rounded-xl'>
+    <div className='w-full min-h-[100vh] bg-[#e0f1e7] flex flex-col justify-center items-center font-body font-normal relative overflow-hidden'>
+      <div className='w-[95%] max-w-[600px] my-8 px-8 py-6 bg-white flex flex-col justify-center items-start rounded-xl'>
         <Form />
       </div>
     </div>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -42,47 +42,52 @@ const Form = () => {
 
 
     return (
-        <form className="relative">
-            <h1 className='text-2xl mb-4'>Contact Us</h1>
-            <div className='w-full flex flex-col lg:flex-row justify-center items-center'>
-                <div className="w-full flex flex-col justify-center items-start">
-                    <label htmlFor="firstName">First Name</label>
-                    <input className='w-full py-2 pl-4' type="text" id="firstName" name="firstName" />
+        <form className="text-[#2b4246]">
+            <h1 className='text-3xl font-bold mb-6'>Contact Us</h1>
+            <div className='w-full flex flex-col lg:flex-row justify-center items-center mb-4'>
+                <div className="w-full flex flex-col justify-center items-start mb-4">
+                    <label className="mb-2" htmlFor="firstName">First Name</label>
+                    <input className='w-full py-2 pl-4 border border-[#2b4246] rounded-md' type="text" id="firstName" name="firstName" />
                     <p className='hidden'>This field is required</p>
                 </div>
                 <div className="w-full flex flex-col justify-center items-start">
-                    <label htmlFor="lastName">Last Name</label>
-                    <input className='w-full py-2 pl-4' type="text" id="lastName" name="lastName" />
+                    <label className="mb-2" htmlFor="lastName">Last Name</label>
+                    <input className='w-full py-2 pl-4 border border-[#2b4246] rounded-md' type="text" id="lastName" name="lastName" />
                     <p className='hidden'>This field is required</p>
                 </div>
             </div>
-            <div>
-                <label htmlFor="email">Email Address</label>
-                <input name="email" type="text" id="email" />
+            <div className="w-full flex flex-col justify-center items-start mb-4">
+                <label className="mb-2" htmlFor="email">Email Address</label>
+                <input className='w-full py-2 pl-4 border border-[#2b4246] rounded-md' type="text" id="email" name="email" />
                 <p className='hidden'>Please enter a valid email address</p>
                 <p className='hidden'>This field is required</p>
             </div>
-            <div>
-                <label htmlFor="">Query Type</label>
-                <div className='flex flex-col lg:flex-row gap-4 my-4'>
-                    <div className='flex flex-row'><input type="radio" name="queryType" id="" /><p className='ml-2'>General Enquiry</p></div>
-                    <div className='flex flex-row'><input type="radio" name="queryType" id="" /><p className='ml-2'>Support Request</p></div>
+            <div className="w-full flex flex-col justify-center items-start mb-4">
+                <label className="mb-2" htmlFor="queryType">Query Type</label>
+                <div className='w-full flex flex-col lg:flex-row gap-4 mb-4'>
+                    <div className='flex flex-row py-3 px-4 border border-[#2b4246] rounded-md'>
+                        <input type="radio" name="queryType" id="queryType" />
+                        <p className='ml-2'>General Enquiry</p>
+                    </div>
+                    <div className='flex flex-row py-3 px-4 border border-[#2b4246] rounded-md'>
+                        <input type="radio" name="queryType" id="" />
+                        <p className='ml-2'>Support Request</p></div>
                 </div>
                 <p className='hidden'>Please select a query type</p>
             </div>                              
-            <div>
-                <label htmlFor="message">Message</label>
-                <textarea className='w-full' id="message" rows="5" name="message"></textarea>
+            <div className="w-full flex flex-col justify-center items-start mb-4">
+                <label className="mb-2" htmlFor="message">Message</label>
+                <textarea className='w-full border border-[#2b4246] rounded-md' id="message" rows="8" name="message"></textarea>
                 <p className='hidden'>This field is required</p>
             </div>
-            <div>
+            <div className="my-4">
                 <div className='flex flex-row'>
                     <input type="checkbox" name="consent" id="consent" />
                     <p className='ml-4'>I consent to being contacted by the team</p>
                 </div>
                 <p className='hidden'>To submit this form, please consent to being contacted</p>
             </div>
-            <button onClick={handleSubmit}>Submit</button>
+            <button className="w-full mt-4 py-3 rounded-md bg-[#0c7d69] text-white text-lg text-center" onClick={handleSubmit}>Submit</button>
             {emailSubmitted&&<ConfirmationMessage />}
         </form>
     )
