9d167d46e659d84d1cfd319688ea1088f6e1179a
JuanMiranda1998 <mirandajuan1998@outlook.com>
2024-06-01 12:21:31 -0300

    styled checkbox input

--- src/components/ConfirmationMessage.jsx
+++ src/components/ConfirmationMessage.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
 
 const ConfirmationMessage = () => {
   return (
     <div className='w-[300px] lg:w-[400px] px-4 py-4 rounded-lg absolute top-1 left-1/2 -translate-x-1/2 bg-[#2a4244ef] text-white'>
-        <h1 className='mb-2'>Message Sent!</h1>
+        <div className='flex flex-row items-center mb-2'>
+          <div>
+            <img src="/icon-success-check.svg" alt="" />
+          </div>
+          <h1 className='ml-2'>Message Sent!</h1>
+        </div>
         <p className='text-sm text-[#e0f1e7]'>Thanks for completing the form. We'll be in touch soon!</p>
     </div>
   )
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,6 @@
+import './Form.css'
 import { useState } from "react"
 import ConfirmationMessage from "./ConfirmationMessage"
-
 
 const Form = () => {
     const [emailSubmitted, setEmailSubmitted] = useState(false)
@@ -87,52 +87,56 @@ const Form = () => {
 
 
     return (
-        <form className="text-[#2b4246]" onSubmit={handleSubmit}>
+        <form className="w-full text-[#2b4246]" onSubmit={handleSubmit}>
             <h1 className='text-3xl font-bold mb-6'>Contact Us</h1>
             <div className='w-full flex flex-col lg:flex-row justify-center items-center mb-4'>
                 <div className="w-full flex flex-col justify-center items-start mb-4 lg:mb-0 lg:mr-2">
-                    <label className="mb-2" htmlFor="firstName">First Name <span>*</span></label>
+                    <label className="mb-2" htmlFor="firstName">First Name <span className="text-[#0c7d69]">*</span></label>
                     <input className='w-full py-2 pl-4 border border-[#2b4246] rounded-md' type="text" id="firstName" name="firstName" onChange={handleChange} value={formData.firstName} />
-                    {isEmpty(formData.firstName) && <p className="my-2 text-sm text-[#d94545]">This field is required</p>}
+                    <div className="h-6">{isEmpty(formData.firstName) && <p className="my-2 text-sm text-[#d94545]">This field is required</p>}</div>
                 </div>
                 <div className="w-full flex flex-col justify-center items-start">
-                    <label className="mb-2" htmlFor="lastName">Last Name <span>*</span></label>
+                    <label className="mb-2" htmlFor="lastName">Last Name <span className="text-[#0c7d69]">*</span></label>
                     <input className='w-full py-2 pl-4 border border-[#2b4246] rounded-md' type="text" id="lastName" name="lastName" onChange={handleChange} value={formData.lastName} />
-                    {isEmpty(formData.lastName) && <p className="my-2 text-sm text-[#d94545]">This field is required</p>}
+                    <div className="h-6">{isEmpty(formData.lastName) && <p className="my-2 text-sm text-[#d94545]">This field is required</p>}</div>
                 </div>
             </div>
             <div className="w-full flex flex-col justify-center items-start mb-4">
-                <label className="mb-2" htmlFor="email">Email Address <span>*</span></label>
+                <label className="mb-2" htmlFor="email">Email Address <span className="text-[#0c7d69]">*</span></label>
                 <input className='w-full py-2 pl-4 border border-[#2b4246] rounded-md' type="text" id="email" name="email" onChange={handleChange} value={formData.email} />
                 {isEmpty(formData.email) && <p className="my-2 text-sm text-[#d94545]">This field is required</p>}
                 {!isEmpty(formData.email) && !verifyEmail(formData.email) && <p className="my-2 text-sm text-[#d94545]">Please enter a valid email address</p>}
             </div>
             <div className="w-full flex flex-col justify-center items-start mb-4">
-                <label className="mb-2" htmlFor="queryType">Query Type <span>*</span></label>
+                <label className="mb-2" htmlFor="queryType">Query Type <span className="text-[#0c7d69]">*</span></label>
                 <div className='w-full flex flex-col lg:flex-row gap-4 mb-4 lg:mb-0'>
-                    <div className='lg:w-full flex flex-row py-3 px-4 border border-[#2b4246] rounded-md'>
+                    <div className='lg:w-full flex flex-row py-3 px-4 border border-[#2b4246] rounded-md cursor-pointer'>
                         <input type="radio" name="queryType" value="generalEnquiry" onClick={handleChange} />
                         <p className='ml-2'>General Enquiry</p>
                     </div>
-                    <div className='lg:w-full flex flex-row py-3 px-4 border border-[#2b4246] rounded-md'>
+                    <div className='lg:w-full flex flex-row py-3 px-4 border border-[#2b4246] rounded-md cursor-pointer'>
                         <input type="radio" name="queryType" value="supportRequest" onClick={handleChange} />
-                        <p className='ml-2'>Support Request</p></div>
+                        <p className='ml-2'>Support Request</p>
+                    </div>
                 </div>
                 {isEmpty(formData.queryType) && <p className="my-2 text-sm text-[#d94545]">Please select a query type</p>}
             </div>                              
             <div className="w-full flex flex-col justify-center items-start mb-4">
-                <label className="mb-2" htmlFor="message">Message <span>*</span></label>
+                <label className="mb-2" htmlFor="message">Message <span className="text-[#0c7d69]">*</span></label>
                 <textarea className='w-full border border-[#2b4246] rounded-md' id="message" rows="8" name="message" onChange={handleChange} value={formData.message}></textarea>
                 {isEmpty(formData.message) && <p className="my-2 text-sm text-[#d94545]">This field is required</p>}
             </div>
             <div className="my-4">
-                <div className='flex flex-row'>
+
+                <label className="inputCheckContainer">
                     <input type="checkbox" name="consent" id="consent" onChange={handleCheckbox} value={formData.consent} />
-                    <p className='ml-4'>I consent to being contacted by the team <span>*</span></p>
-                </div>
+                    <span className="checkmark"></span>
+                    <p>I consent to being contacted by the team <span className="text-[#0c7d69]">*</span></p>
+                </label>
+
                 {!formData.consent && <p className="my-2 text-sm text-[#d94545]">To submit this form, please consent to being contacted</p>}
             </div>
-            <button className="w-full mt-4 py-3 rounded-md bg-[#0c7d69] text-white text-lg text-center">Submit</button>
+            <button className="w-full mt-4 py-3 rounded-md bg-[#0c7d69] text-white text-lg text-center hover:bg-[#063f36] transition-all">Submit</button>
             {emailSubmitted&&<ConfirmationMessage />}
         </form>
     )
