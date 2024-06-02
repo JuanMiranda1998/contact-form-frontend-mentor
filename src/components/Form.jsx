05827e88dd68de85853c0a54602a39a4bada9d8b
JuanMiranda1998 <mirandajuan1998@outlook.com>
2024-06-02 07:59:15 -0300

    added hover and focus states

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -92,38 +92,38 @@ const Form = () => {
             <div className='w-full flex flex-col lg:flex-row justify-center items-center mb-4'>
                 <div className="w-full flex flex-col justify-center items-start mb-4 lg:mb-0 lg:mr-2">
                     <label className="mb-2" htmlFor="firstName">First Name <span className="text-[#0c7d69]">*</span></label>
-                    <input className='w-full py-2 pl-4 border border-[#2b4246] rounded-md' type="text" id="firstName" name="firstName" onChange={handleChange} value={formData.firstName} />
+                    <input className='w-full py-2 pl-4 border border-[#2b4246] rounded-md cursor-pointer hover:border-[#0c7d69] focus:outline-none focus:border-2 focus:border-[#0c7d69]' type="text" id="firstName" name="firstName" onChange={handleChange} value={formData.firstName} />
                     <div className="h-6">{isEmpty(formData.firstName) && <p className="my-2 text-sm text-[#d94545]">This field is required</p>}</div>
                 </div>
                 <div className="w-full flex flex-col justify-center items-start">
                     <label className="mb-2" htmlFor="lastName">Last Name <span className="text-[#0c7d69]">*</span></label>
-                    <input className='w-full py-2 pl-4 border border-[#2b4246] rounded-md' type="text" id="lastName" name="lastName" onChange={handleChange} value={formData.lastName} />
+                    <input className='w-full py-2 pl-4 border border-[#2b4246] rounded-md cursor-pointer hover:border-[#0c7d69] focus:outline-none focus:border-2 focus:border-[#0c7d69]' type="text" id="lastName" name="lastName" onChange={handleChange} value={formData.lastName} />
                     <div className="h-6">{isEmpty(formData.lastName) && <p className="my-2 text-sm text-[#d94545]">This field is required</p>}</div>
                 </div>
             </div>
             <div className="w-full flex flex-col justify-center items-start mb-4">
                 <label className="mb-2" htmlFor="email">Email Address <span className="text-[#0c7d69]">*</span></label>
-                <input className='w-full py-2 pl-4 border border-[#2b4246] rounded-md' type="text" id="email" name="email" onChange={handleChange} value={formData.email} />
+                <input className='w-full py-2 pl-4 border border-[#2b4246] rounded-md cursor-pointer hover:border-[#0c7d69] focus:outline-none focus:border-2 focus:border-[#0c7d69]' type="text" id="email" name="email" onChange={handleChange} value={formData.email} />
                 {isEmpty(formData.email) && <p className="my-2 text-sm text-[#d94545]">This field is required</p>}
                 {!isEmpty(formData.email) && !verifyEmail(formData.email) && <p className="my-2 text-sm text-[#d94545]">Please enter a valid email address</p>}
             </div>
             <div className="w-full flex flex-col justify-center items-start mb-4">
                 <label className="mb-2" htmlFor="queryType">Query Type <span className="text-[#0c7d69]">*</span></label>
                 <div className='w-full flex flex-col lg:flex-row gap-4 mb-4 lg:mb-0'>
-                    <div className='lg:w-full flex flex-row py-3 px-4 border border-[#2b4246] rounded-md cursor-pointer'>
-                        <input type="radio" name="queryType" value="generalEnquiry" onClick={handleChange} />
+                    <label htmlFor='generalEnquiry' className='lg:w-full flex flex-row py-3 px-4 border border-[#2b4246] rounded-md cursor-pointer hover:border-[#0c7d69] hover:bg-[#e0f1e7] transition-all focus:outline-none focus:border-2 focus:border-[#0c7d69]'>
+                        <input type="radio" name="queryType" id='generalEnquiry' value="generalEnquiry" onClick={handleChange} />
                         <p className='ml-2'>General Enquiry</p>
-                    </div>
-                    <div className='lg:w-full flex flex-row py-3 px-4 border border-[#2b4246] rounded-md cursor-pointer'>
-                        <input type="radio" name="queryType" value="supportRequest" onClick={handleChange} />
+                    </label>
+                    <label htmlFor='supportRequest' className='lg:w-full flex flex-row py-3 px-4 border border-[#2b4246] rounded-md cursor-pointer hover:border-[#0c7d69] hover:bg-[#e0f1e7] transition-all focus:outline-none focus:border-2 focus:border-[#0c7d69]'>
+                        <input type="radio" name="queryType" id='supportRequest' value="supportRequest" onClick={handleChange} />
                         <p className='ml-2'>Support Request</p>
-                    </div>
+                    </label>
                 </div>
                 {isEmpty(formData.queryType) && <p className="my-2 text-sm text-[#d94545]">Please select a query type</p>}
             </div>                              
             <div className="w-full flex flex-col justify-center items-start mb-4">
                 <label className="mb-2" htmlFor="message">Message <span className="text-[#0c7d69]">*</span></label>
-                <textarea className='w-full border border-[#2b4246] rounded-md' id="message" rows="8" name="message" onChange={handleChange} value={formData.message}></textarea>
+                <textarea className='w-full px-4 py-2 border border-[#2b4246] rounded-md cursor-pointer hover:border-[#0c7d69] focus:outline-none focus:border-2 focus:border-[#0c7d69]' id="message" rows="4" name="message" onChange={handleChange} value={formData.message}></textarea>
                 {isEmpty(formData.message) && <p className="my-2 text-sm text-[#d94545]">This field is required</p>}
             </div>
             <div className="my-4">
