d09025d6eed814c5d9cb7b8fafde682dd7f051b6
JuanMiranda1998 <mirandajuan1998@outlook.com>
2024-06-19 12:05:42 -0300

    added screenshots

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ This is a solution to the [Contact form challenge on Frontend Mentor](https://ww
 
 - [Overview](#overview)
 - [The challenge](#the-challenge)
+- [Screenshot](#screenshot)
 - [Links](#links)
 - [My process](#my-process)
 - [Built with](#built-with)
@@ -27,6 +28,11 @@ Users should be able to:
 - Have inputs, error messages, and the success message announced on their screen reader
 - View the optimal layout for the interface depending on their device's screen size
 - See hover and focus states for all interactive elements on the page
+
+
+### Screenshot
+![Desktop(1440px)](./capture/capture-desktop.png)
+![Mobile(375px)](./capture/capture-mobile.png)
 
 
 ### Links
--- src/components/FormMessage.jsx
+++ src/components/FormMessage.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const FormMessage = ({ id, name, formData, handleChange, errors, errorActive, formTriggered}) => {
-
+  const [desktopWindowSize, setDesktopWindowSize] = useState(window.matchMedia("(min-width: 768px)").matches) 
   const [focused, setFocused] = useState(false)
 
     const isInvalid = (errorActive!='' && focused) || (errorActive!='' && formTriggered);
@@ -10,10 +10,19 @@ const FormMessage = ({ id, name, formData, handleChange, errors, errorActive, fo
         setFocused(true)
     }
 
+    
+
+    useEffect(() => {
+        window.matchMedia("(min-width: 768px)").addEventListener('change', () => {
+        setDesktopWindowSize(window.matchMedia("(min-width: 768px)").matches)
+      });
+    } , [])
+     
+
   return (
     <div className="w-full flex flex-col justify-center items-start mb-4">
         <label className="mb-2" htmlFor="message">Message <span className="text-[#0c7d69]">*</span></label>
-        <textarea className='w-full px-4 py-2 border border-[#2b4246] rounded-md cursor-pointer hover:border-[#0c7d69] focus:outline-none focus:border-2 focus:border-[#0c7d69]' id={id} rows="4" name={name} onChange={handleChange} value={formData} onBlur={handleFocus} focused={focused.toString()}>
+        <textarea className='w-full px-4 py-2 border border-[#2b4246] rounded-md cursor-pointer hover:border-[#0c7d69] focus:outline-none focus:border-2 focus:border-[#0c7d69]' id={id} rows={desktopWindowSize ? 4 : 9} name={name} onChange={handleChange} value={formData} onBlur={handleFocus} focused={focused.toString()}>
         </textarea>
         <div>
         {errors.map((error, index) => (  
